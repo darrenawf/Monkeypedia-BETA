a28d8622c3b484ea258b32b7f47651f7bf25d735
WesleyDang <149202418+WesleyDang@users.noreply.github.com>
2023-11-12 01:16:44 -0500

    Update Monkeypedia.js

--- Monkeypedia.js
+++ Monkeypedia.js
@@ -46,3 +46,65 @@ function checkDiet() { // Diet filter checkbox
     document.getElementById("speciesList").style.display = "none";
 }
 /*FILTERS=============================================*/
+
+/*ACCOUNT=============================================*/
+function createAccount(){ //Finish this later when we learn about node.js
+    /*
+
+    var newUser = document.getElementById("newUser").value;
+    var newPass = document.getElementById("newPass").value;
+    alert("newUser");
+    const fs = require('fs');
+
+    let fileUser = fs.readFileSync(usernames.txt).toString();
+    let filePass = fs.readFileSync(passwords.txt).toString();
+    const userArray = fileUser.split("");
+    const passArray = fileUser.split("");
+    
+    for (let i = 0; i < userArray.length;i++){
+        
+        if (userArray[i] == newUser){
+            alert("That username has been taken. Please enter a new username")
+            return;
+        }
+    }
+    alert("Account has been created")
+    userArray.push(newUser);
+    passArray.push(newPass);
+    fileUser = userArray.join("");
+    filePass = passArray.join("");
+    fs.writeFileSync("usernames.txt", fileUser)
+    fs.writeFileSync("passwords.txt", filePass)
+    */
+    
+
+
+}
+/*ACCOUNT=============================================*/
+
+
+/*SEARCH=============================================*/
+//include all monkeys here, thanks.
+var monkeyArray = ["Aye-aye", "Baboon", "Chimpanzee", "Douc", "Finger Monkey", "Gorilla", "Indri", "Japanese Macaque", "Lar Gibbon", "Mandrill", "Orangutan", "Proboscis", "Siamang", "Snub-Nosed Monkey", "Spider Monkey", "Squirrel Monkey", "Woolly Monkey"];
+
+function search(){
+    var input = document.getElementById("search").value;
+    window.location.href = 'search_results.html?input=' + encodeURIComponent(input);
+}
+
+function results(input){
+    document.getElementById("resultsTitle").innerHTML = "Search Results for: \"" + input + "\"";
+    input = input.toLowerCase();
+    var output = "";
+    for (i = 0; i < (monkeyArray.length);i++){
+        monkey = monkeyArray[i].toLowerCase(); //I made them both lower case so it's not case sensitive
+        if (monkey.includes(input) == true){
+            alert(monkey);
+            link = '"' + monkey+ '.html"';
+            output = output + "<a href="+link+">"+monkeyArray[i]+"</a>" //creates the <a> tag with a working link
+        }
+    }
+    document.getElementById("results").innerHTML = output;
+}
+/*SEARCH=============================================*/
+
