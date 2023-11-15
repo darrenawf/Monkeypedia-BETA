89921799d714c2e6b6506db0d7a0b752eba145f2
Darren <darrenawf@gmail.com>
2023-11-15 14:06:33 -0500

    Scroll images on chimp page

--- Monkeypedia.js
+++ Monkeypedia.js
@@ -1,5 +1,29 @@
+/*
+Type here
+*/
+var chimpanzeeImg = [
+    "images/chimpanzee_01.jpg",
+    "images/chimpanzee_02.jpg",
+    "images/chimpanzee_03.jpg",
+];
+var scrollImg = 0;
 /*SLIDE IMAGES========================================*/
-//function{}
+function scrollLeft() {
+    scrollImg--;
+    if (scrollImg < 0) {
+        scrollImg = chimpanzeeImg.length - 1;
+    }
+    var monkeyImg = document.querySelector(".monkeyImg");
+    monkeyImg.src = chimpanzeeImg[scrollImg];
+}
+function scrollRight() {
+    scrollImg++;
+    if (scrollImg >= chimpanzeeImg.length) {
+        scrollImg = 0;
+    }
+    var monkeyImg = document.querySelector(".monkeyImg");
+    monkeyImg.src = chimpanzeeImg[scrollImg];
+}
 /*SLIDE IMAGES========================================*/
 /*FILTERS=============================================*/
 function checkRegion() { // Region filter checkbox
@@ -48,7 +72,7 @@ function checkDiet() { // Diet filter checkbox
 /*FILTERS=============================================*/
 
 /*ACCOUNT=============================================*/
-function createAccount(){ //Finish this later when we learn about node.js
+function createAccount() { //Finish this later when we learn about node.js
     /*
 
     var newUser = document.getElementById("newUser").value;
@@ -76,7 +100,7 @@ function createAccount(){ //Finish this later when we learn about node.js
     fs.writeFileSync("usernames.txt", fileUser)
     fs.writeFileSync("passwords.txt", filePass)
     */
-    
+
 
 
 }
@@ -87,20 +111,20 @@ function createAccount(){ //Finish this later when we learn about node.js
 //include all monkeys here, thanks.
 var monkeyArray = ["Aye-aye", "Baboon", "Chimpanzee", "Douc", "Finger Monkey", "Gorilla", "Indri", "Japanese Macaque", "Lar Gibbon", "Mandrill", "Orangutan", "Proboscis", "Siamang", "Snub-Nosed Monkey", "Spider Monkey", "Squirrel Monkey", "Woolly Monkey"];
 
-function search(){
+function search() {
     var input = document.getElementById("search").value;
     window.location.href = 'search_results.html?input=' + encodeURIComponent(input);
 }
 
-function results(input){
+function results(input) {
     document.getElementById("resultsTitle").innerHTML = "Search Results for: \"" + input + "\"";
     input = input.toLowerCase();
     var output = "";
-    for (i = 0; i < (monkeyArray.length);i++){
+    for (i = 0; i < (monkeyArray.length); i++) {
         monkey = monkeyArray[i].toLowerCase(); //I made them both lower case so it's not case sensitive
-        if (monkey.includes(input) == true){
-            link = '"' + monkey+ '.html"';
-            output = output + "<a href="+link+">"+monkeyArray[i]+"</a>" //creates the <a> tag with a working link, Note: The link most likely doesnt work with monkey names with spaces, but I can easily fix that later
+        if (monkey.includes(input) == true) {
+            link = '"' + monkey + '.html"';
+            output = output + "<a href=" + link + ">" + monkeyArray[i] + "</a>" //creates the <a> tag with a working link, Note: The link most likely doesnt work with monkey names with spaces, but I can easily fix that later
         }
     }
     document.getElementById("results").innerHTML = output;
